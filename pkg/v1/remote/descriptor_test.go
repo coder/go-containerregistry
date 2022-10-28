754d74ca65e81b8aae02b45456da2ac66b073c46
Cian Johnston <cian@coder.com>
2022-10-29 00:30:48 +0100

    add unit test for fetchBlob

--- pkg/v1/remote/descriptor_test.go
+++ pkg/v1/remote/descriptor_test.go
@@ -15,8 +15,10 @@
 package remote
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"net/url"
@@ -25,6 +27,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 	"github.com/google/go-containerregistry/pkg/v1/types"
 )
 
@@ -216,3 +219,71 @@ func TestHead_MissingHeaders(t *testing.T) {
 		}
 	}
 }
+
+// TestFetchBlobVerifyContentLength tests that fetchBlob closes its response
+// body if the remote server returns an incorrect value for the Content-Length
+// header.
+func TestFetchBlobVerifyContentLength(t *testing.T) {
+	ctx := context.Background()
+	rc := &testReadCloser{Closed: false, r: strings.NewReader(" ")}
+	tr := &fakeTransport{
+		ContentLengthHeaderValue: "2",
+		Body:                     rc,
+	}
+	f := fetcher{
+		Ref: mustNewTag(t, "original.com/repo:latest"),
+		Client: &http.Client{
+			Transport: tr,
+		},
+		context: ctx,
+	}
+	h, err := v1.NewHash("sha256:0000000000000000000000000000000000000000000000000000000000000000")
+	if err != nil {
+		t.Fatal("NewHash:", err)
+	}
+	if _, err := f.fetchBlob(ctx, 1, h); err == nil {
+		t.Errorf("fetchBlob: expected error, got nil")
+	} else if !strings.Contains(err.Error(), "Content-Length header 0 does not match expected size 1") {
+		t.Errorf("fetchBlob: expected content-length mismatch error, got %v", err)
+	}
+	if !rc.Closed {
+		t.Errorf("fetchBlob: expected to close response body")
+	}
+}
+
+// fakeTransport returns a response with the given Content-Length header and body.
+type fakeTransport struct {
+	ContentLengthHeaderValue string
+	Body                     io.ReadCloser
+}
+
+func (t *fakeTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	return &http.Response{
+		StatusCode: http.StatusOK,
+		Header: http.Header{
+			http.CanonicalHeaderKey("content-length"): []string{t.ContentLengthHeaderValue},
+		},
+		Body: t.Body,
+	}, nil
+}
+
+var _ http.RoundTripper = (*fakeTransport)(nil)
+
+type testReadCloser struct {
+	Closed bool
+	r      io.Reader
+}
+
+func (rc *testReadCloser) Close() error {
+	rc.Closed = true
+	return nil
+}
+
+func (rc *testReadCloser) Read(p []byte) (n int, err error) {
+	if rc.Closed {
+		return 0, io.EOF
+	}
+	return rc.r.Read(p)
+}
+
+var _ io.ReadCloser = (*testReadCloser)(nil)
